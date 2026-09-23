00c334995c6a0d1cdbbe9148d45e4b14dc3676bb
Theeraphong Chanathipat <69010520@kmitl.ac.th>
2026-09-23 17:07:03 +0700

    add greeting function and wdit main

--- Hello.c
+++ Hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-    printf("Hello, Git Collaboration!\n");
+    greeting("Developer A,B,C !!");
+    return 0;
+}
+int greeting(const chat *name)
+{
+    printf("Hello, %s!\n", name);
     return 0;
 }
